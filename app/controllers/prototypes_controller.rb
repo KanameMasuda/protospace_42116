d5bc61d233ab283c7e4133b52577a78c5008e801
KanameMasuda <crossover_dragon@yahoo.co.jp>
2025-03-12 20:29:16 +0900

    before_action :move_to_indexに:newを追加

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :new]
 
   def index
     @prototypes = Prototype.all
