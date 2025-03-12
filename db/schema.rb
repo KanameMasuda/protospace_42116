eb7fa99b77a2683b3d58f43bd7ed4f26bbac6103
KanameMasuda <crossover_dragon@yahoo.co.jp>
2025-03-12 20:09:33 +0900

    マイグレーション修正

--- db/schema.rb
+++ db/schema.rb
@@ -62,15 +62,15 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_103412) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "name", null: false
+    t.text "profile", null: false
+    t.text "occupation", null: false
+    t.text "position", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
-    t.text "profile"
-    t.text "occupation"
-    t.text "position"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
