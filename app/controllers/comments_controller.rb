83ab2c14e2df8f1969a750d813f629b49737e45c
KanameMasuda <crossover_dragon@yahoo.co.jp>
2025-03-13 10:56:46 +0900

    コメントフォームを空のまま投稿修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -9,6 +9,8 @@ class CommentsController < ApplicationController
     if @comment.save
       redirect_to prototype_path(@prototype)
     else
+      @prototype = @comment.prototype
+      @comments = @prototype.comments.includes(:user)
       render 'prototypes/show'
     end
   end
